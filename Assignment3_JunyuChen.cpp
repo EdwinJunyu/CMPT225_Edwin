012ae327668ad0a3dae3627126d8ae8c8e8fdd34
EdwinJunyu <H1111236_Chen>
2026-03-14 16:16:54 -0700

    A3 Question A | manu display

--- Assignment3_JunyuChen.cpp
+++ Assignment3_JunyuChen.cpp
@@ -52,7 +52,10 @@ private:
     }
 
 public:
-    MyTree();
+    MyTree(){
+        root = nullptr;
+        size = 0;
+    };
     ~MyTree(){};
     bool isEmpty() const{
         return root == nullptr;
@@ -174,7 +177,7 @@ public:
         Node<T>* cur = find(data);
         if (cur == nullptr) return -1;
         int cnt = 0;
-        while(cur != data){
+        while(cur != root){
             cnt++;
             cur = cur->parent;
         }
@@ -215,18 +218,127 @@ public:
     }
 };
 
+int main() {
+    MyTree<string> familyTree;
+    char choice;
 
+    while (true) {
+        cout << endl;
+        cout << "========== Family Tree Menu ==========" << endl;
+        cout << "a. Add root" << endl;
+        cout << "b. Add child" << endl;
+        cout << "c. Search" << endl;
+        cout << "d. Number of children" << endl;
+        cout << "e. Depth" << endl;
+        cout << "f. Remove subtree" << endl;
+        cout << "g. Print traversal" << endl;
+        cout << "h. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
+        if (choice == 'a' || choice == 'A') {
+            string name;
+            cout << "Enter root name: ";
+            cin >> name;
 
+            if (familyTree.addRoot(name)) {
+                cout << "Root added successfully." << endl;
+            } else {
+                cout << "Cannot add root. Tree already has a root." << endl;
+            }
+        }
+        else if (choice == 'b' || choice == 'B') {
+            string parentName;
+            string childName;
 
+            cout << "Enter parent name: ";
+            cin >> parentName;
+            cout << "Enter child name: ";
+            cin >> childName;
 
+            if (familyTree.addChild(parentName, childName)) {
+                cout << "Child added successfully." << endl;
+            } else {
+                cout << "Cannot add child. Parent not found, tree empty, or child name already exists." << endl;
+            }
+        }
+        else if (choice == 'c' || choice == 'C') {
+            string name;
+            cout << "Enter name to search: ";
+            cin >> name;
 
+            if (familyTree.find(name) != nullptr) {
+                cout << name << " exists in the tree." << endl;
+            } else {
+                cout << name << " does not exist in the tree." << endl;
+            }
+        }
+        else if (choice == 'd' || choice == 'D') {
+            string name;
+            cout << "Enter name: ";
+            cin >> name;
 
+            cout << "Number of children of " << name << ": "
+                 << familyTree.numChildren(name) << endl;
+        }
+        else if (choice == 'e' || choice == 'E') {
+            string name;
+            cout << "Enter name: ";
+            cin >> name;
 
+            int d = familyTree.depth(name);
 
+            if (d == -1) {
+                cout << "Name not found." << endl;
+            } else {
+                cout << "Depth of " << name << ": " << d << endl;
+            }
+        }
+        else if (choice == 'f' || choice == 'F') {
+            string name;
+            cout << "Enter name of subtree root to remove: ";
+            cin >> name;
 
+            if (familyTree.removeSubtree(name)) {
+                cout << "Subtree removed successfully." << endl;
+            } else {
+                cout << "Cannot remove subtree. Name not found." << endl;
+            }
+        }
+        else if (choice == 'g' || choice == 'G') {
+            char traversalType;
 
+            cout << "Choose traversal type:" << endl;
+            cout << "P for preorder" << endl;
+            cout << "O for postorder" << endl;
+            cout << "L for level-order" << endl;
+            cout << "Enter traversal type: ";
+            cin >> traversalType;
 
+            if (traversalType == 'P' || traversalType == 'p') {
+                cout << "Preorder: ";
+                familyTree.printPreorder();
+            }
+            else if (traversalType == 'O' || traversalType == 'o') {
+                cout << "Postorder: ";
+                familyTree.printPostorder();
+            }
+            else if (traversalType == 'L' || traversalType == 'l') {
+                cout << "Level-order: ";
+                familyTree.printByLevels();
+            }
+            else {
+                cout << "Invalid traversal type." << endl;
+            }
+        }
+        else if (choice == 'h' || choice == 'H') {
+            cout << "Exiting program." << endl;
+            break;
+        }
+        else {
+            cout << "Invalid menu choice." << endl;
+        }
+    }
 
-
-
+    return 0;
+}
